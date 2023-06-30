511f399f86b9e82a5de02d9afed978ca0d6bf84d
Jordan-Lowe <jordanlowe659@gmail.com>
2023-06-30 18:58:31 +1200

    Praying it is donegit pull origin main! and all changes are right

--- client/apis/promises.ts
+++ client/apis/promises.ts
@@ -1,7 +1,22 @@
+import request from 'superagent'
+import { Pledge, PledgeFrontEnd } from '../../models/pledge_models'
 
-export async function getPromiseByPromiseId(promiseId: number, token: string): Promise<PledgeFrontEnd> {
+const rootUrl = 'api/v1/promises'
+
+export async function getAllPromisesByUserId(token: string): Promise<Pledge[]> {
   const res = await request
-  .get(rootUrl + `/` + promiseId)
-  .set('Authorization', `Bearer ${token}`)
-    return res.body
+    .get(rootUrl)
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json')
+  return res.body as Pledge[]
+}
+
+export async function getPromiseByPromiseId(
+  promiseId: number,
+  token: string
+): Promise<PledgeFrontEnd> {
+  const res = await request
+    .get(rootUrl + `/` + promiseId)
+    .set('Authorization', `Bearer ${token}`)
+  return res.body
 }
--- server/db/dataBaseFunctions/promisesDB.ts
+++ server/db/dataBaseFunctions/promisesDB.ts
@@ -5,6 +5,20 @@ export function getAllPromises(db = connection): Promise<Pledge[]> {
   return db('promises').select()
 }
 
+export function getPromisesById(id: string, db = connection): Promise<Pledge[]> {
+  return db('promises')
+    .where('id', id)
+    .select(
+      'promise_name as promiseName',
+      'promise_description as promiseDescription',
+      'user_id as userId',
+      'friend_user_id as friendUserId',
+      'status',
+      'date_created as dateCreated',
+      'date_due as dateDue'
+    )
+}
+
 export function getPromiseByIdWithFriendName(
   id: number,
   db = connection
